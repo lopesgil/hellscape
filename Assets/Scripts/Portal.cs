d70c63906e26ed0d19d36ce969e4db82a49993cb
Renanpulga <renaalmeid@hotmail.com>
2017-05-24 15:03:26 -0300

    Cutscene funcional
    Cutscene ainda simples, provavelmente irei alterar a fonte e deixar mais fluida e interativa

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -10,30 +10,47 @@ public class Portal : MonoBehaviour {
     public int moedasPraCompletar;
     public float counter;
     public float tempomax04;
+
+    enum CurrentStage
+    {
+        ganancia,
+        luxuria,
+        ira,
+        orgulho,
+        inveja,
+        gula,
+        preguica
+    }
+    [SerializeField]
+    CurrentStage currentstage;
 	// Use this for initialization
 	void Start () {
-        aberto = false;
         spriteRenderer = GetComponent<SpriteRenderer>();
         counter = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (GameObject.FindWithTag("Moeda") != null)
+
+    // Update is called once per frame
+    void Update() {
+        switch (currentstage)
         {
-            if (PlayerPlatformerController.count >= moedasPraCompletar) aberto = true;
-        }
-        else
-        {
-            string faseAtual = SceneManager.GetActiveScene().name;
-            if (faseAtual == "Fase04")
-            {
+            case CurrentStage.ganancia:
+                if (PlayerPlatformerController.count >= moedasPraCompletar) aberto = true;
+                break;
+            case CurrentStage.inveja:
                 counter += Time.deltaTime;
-            }
-            if(counter<=tempomax04)
-            {
-                aberto = true;
-            }
+                if(counter<=tempomax04)
+                {
+                    aberto = true;
+                }
+                break;
+            case CurrentStage.gula:
+                if(PlayerPlatformerController.gula1 && PlayerPlatformerController.gula2 && PlayerPlatformerController.gula3)
+                {
+                    aberto = true;
+                }
+                break;
+            default:
+                break;
         }
         if (aberto)
         {
@@ -44,17 +61,23 @@ public class Portal : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         string proximaFase;
-        string faseAtual = SceneManager.GetActiveScene().name;
-        if (faseAtual == "Fase01")
+        switch(currentstage)
         {
-            proximaFase = "Fase04";
-        } else if(faseAtual == "Fase04")
-        {
-            proximaFase = "Fase07";
-        }
-        else
-        {
-            proximaFase = "Fase01";
+            case CurrentStage.ganancia:
+                proximaFase = "Fase04";
+                break;
+            case CurrentStage.orgulho:
+                proximaFase = "Fase05";
+                break;
+            case CurrentStage.inveja:
+                proximaFase = "Fase06";
+                break;
+            case CurrentStage.gula:
+                proximaFase = "Fase07";
+                break;
+            default:
+                proximaFase = "Fase01";
+                break;
         }
         if (aberto && collision.gameObject.CompareTag("Player"))
         {
